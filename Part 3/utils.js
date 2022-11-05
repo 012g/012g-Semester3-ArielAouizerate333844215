a1758c39f04658fee4dc885fa2b0a1b886e2c0e3
012g <32014508+012g@users.noreply.github.com>
2022-11-05 15:47:20 +0200

    Delete Part 3 directory

--- Part 3/utils.js
+++
@@ -1,5 +0,0 @@
-// A utility function for error handling - like events are full (no availble slots) or trying to join same event whe already in it
-exports.mongooseErrorHandler = (error) => {
-  if (error.errors) var errorMessage = Object.values(error.errors)[0].message;
-  return errorMessage || error.message;
-};
